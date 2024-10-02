c6bf1e518d07274ca303a2b6494df6b07160d47e
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-10-02 14:14:03 +0800

    Create 0050A - Domino piling.cpp

---
+++ 0050A - Domino piling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    short m, n;
+    cin >> m >> n;
+    cout << (m * n) / 2 << endl;
+
+    return 0;
+}
