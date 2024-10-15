91a6fe2707ad1e6992f15b11239cd0bf3f618c67
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-10-15 15:16:51 +0800

    Create 0977A - Wrong Subtraction.cpp

---
+++ 0977A - Wrong Subtraction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    while (k > 0) {
+        if (n % 10 == 0) {
+            n /= 10;
+        } else {
+            n -= 1;
+        }
+        --k;
+    }
+
+    cout << n << endl;
+
+    return 0;
+}
