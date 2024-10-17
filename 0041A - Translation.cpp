b7d27ded26225bd76cf9dfd45e810d6bef05c41e
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-10-17 17:27:30 +0800

    Create 0041A - Translation.cpp

---
+++ 0041A - Translation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    reverse(s.begin(), s.end());
+
+    if (s == t)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
